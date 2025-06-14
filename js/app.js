15b9f683d771bd33a46c713d3ae4c6113c7cbbcf
osaeman <2015n2424@gmail.com>
2025-06-14 20:54:45 +0500

    address change

--- js/app.js
+++ js/app.js
@@ -2,10 +2,8 @@ const icon = document.querySelector(".icon");
 const closebtn = document.querySelector(".closebtn");
 
 icon.addEventListener("click", () => {
-  document.getElementById("mySidenav").style.width = "300px";
-  //document.getElementById("header").style.marginLeft = "250px";
+  document.getElementById("mySidenav").style.width = "280px";
 });
 closebtn.addEventListener("click", () => {
   document.getElementById("mySidenav").style.width = "0";
-  //document.getElementById("header").style.marginLeft= "0";
 });
